5dcbde66060c3a02774c301f23b6629065447b8d
jpcauthor <jpcauthor@gmail.com>
2026-03-18 00:29:37 -0600

    Add files via upload

---
+++ lore/file.bat
@@ -0,0 +1,13 @@
+@echo off
+echo Creating Rivendawn primer files...
+
+type nul > history-primer.html
+type nul > citadels-primer.html
+type nul > regions-primer.html
+type nul > peoples-primer.html
+type nul > faith-belief-primer.html
+type nul > world-today.html
+
+echo.
+echo Files created successfully.
+pause
